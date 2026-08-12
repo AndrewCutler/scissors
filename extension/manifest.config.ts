5e0732ca9926a5b2b7563521a50c4f52ba273a17
Andrew Cutler <AndrewKCutler@gmail.com>
2026-08-12 17:09:51 -0400

    init

---
+++ extension/manifest.config.ts
@@ -0,0 +1,24 @@
+import { defineManifest } from '@crxjs/vite-plugin';
+import pkg from './package.json';
+
+export default defineManifest({
+	manifest_version: 3,
+	name: pkg.name,
+	version: pkg.version,
+	icons: {
+		48: 'public/logo.png',
+	},
+	action: {
+		default_icon: {
+			48: 'public/logo.png',
+		},
+	},
+	permissions: ['sidePanel', 'contentSettings'],
+	side_panel: {
+		default_path: 'src/sidepanel/index.html',
+	},
+	background: {
+		service_worker: 'src/service-workers/serviceWorker.ts',
+		type: 'module',
+	},
+});
